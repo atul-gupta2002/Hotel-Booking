1379a6cdd5d22b0fe4b27a43283aa6554bd9e192
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-07-20 10:54:49 +0530

    Completed the hotels page with the addition of filter section

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -1,18 +1,59 @@
 import React from 'react'
-import {assets, roomsDummyData} from "../assets/assets.js";
+import {assets, facilityIcons, roomsDummyData} from "../assets/assets.js";
 import {useNavigate} from "react-router-dom";
 import StarRating from "../components/StarRating.jsx";
+
+
+const CheckBox = ({label, selected = false, onChange= () => { }}) => {
+    return (
+        <label className='flex gap-3 items-center cursor-pointer mt-2 text-sm'>
+            <input type="checkbox" checked={selected} onChange={(e)=>onChange(e.target.checked,label )} />
+            <span className=' font-light select-none'>{label}</span>
+        </label>
+    )
+}
+
+const RadioButton = ({label, selected = false, onChange= () => { }}) => {
+    return (
+        <label className='flex gap-3 items-center cursor-pointer mt-2 text-sm'>
+            <input type="radio" name="sortOption" checked={selected} onChange={(e)=>onChange(e.target.checked,label )} />
+            <span className=' font-light select-none'>{label}</span>
+        </label>
+    )
+}
 
 const AllRooms = () => {
 
     const navigate = useNavigate();
+    const [openFilters, setOpenFilters] = React.useState(false);
+
+    const roomTypes = [
+        'Single Room',
+        'Double Room',
+        'Luxury Room',
+        'Family Room'
+    ];
+
+    const priceRanges = [
+        '0 to 500',
+        '500 to 1000',
+        '1000 to 2000',
+        '2000 to 3000'
+    ];
+
+    const sortOptions = [
+        "Price Low to High",
+        "Price High to Low",
+        "Newest Arrivals"
+    ];
 
     return (
-        <div className='flex flex-col-reverse items-center lg:flex-row justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+        <div className=' flex flex-col-reverse items-start lg:flex-row justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+
             <div>
                 <div className='flex flex-col items-start text-left'>
                     <h1 className='font-playfair text-4xl md:text-[40px]'>Hotel rooms</h1>
-                    <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>TAke advantage of our limited time offers and special packages to enhance your stay and create unforgettable memories.</p>
+                    <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited time offers and special packages to enhance your stay and create unforgettable memories.</p>
                 </div>
 
                 {roomsDummyData.map((room) => (
@@ -46,14 +87,65 @@ const AllRooms = () => {
                                 <span>{room.hotel.address}</span>
                             </div>
 
+                            <div className='flex flex-wrap items-center gap-4 mt-3 mb-6' >
+                                {room.amenities.map((item, index) => (
+                                    <div key={index} className='flex items-center gap-2 px-3
+                                    py-2 rounded-lg bg-[#F5F5FF]/70'>
+                                        <img src={facilityIcons[item]} alt={item}
+                                        className='w-5 h-5' />
+                                       <p className={'text-xs'}>{item}</p>
+                                    </div>
+
+                                ))}
+                            </div>
+
+                            {/* Room price per night */}
+                            <p className={'text-xl font-medium text-gray-700'}>${room.pricePerNight} /night</p>
+
                         </div>
                     </div>
-
                 ))}
-
             </div>
 
+                {/* Filters */}
+            <div className='bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-2 top-32'>
+                <div className={`flex items-center justify-between px-5 py-2.5 min-lg:border-b border-gray-300 ${openFilters ? "border-b" : ""}`}>
+                    <p className='text-base font-medium text-gray-800'>FILTERS</p>
+                    <div className='text-xs cursor-pointer'>
+            <span onClick={() => setOpenFilters(!openFilters)} className='lg:hidden'>
+                {openFilters ? 'HIDE' : 'SHOW'}
+            </span>
+                        <span className='hidden lg:block cursor-pointer'>CLEAR</span>
+                    </div>
+                </div>
+
+                <div className={`${openFilters ? 'h-auto' : 'h-0 lg:h-auto'} overflow-hidden transition-all`}>
+                    <div className='px-5 pt-5'>
+                        <p className='font-medium text-gray-800 pb-2'>Popular filters</p>
+                        {roomTypes.map((room, index) => (
+                            <CheckBox key={index} label={room} />
+                        ))}
+                    </div>
+
+                    <div className='px-5 pt-5'>
+                        <p className='font-medium text-gray-800 pb-2'>Price range</p>
+                        {priceRanges.map((range, index) => (
+                            <CheckBox label={`$ ${range}`} key={index} />
+                        ))}
+                    </div>
+
+                    <div className='px-5 pt-5 pb-7'>
+                        <p className='font-medium text-gray-800 pb-2'>Sort by</p>
+                        {sortOptions.map((option, index) => (
+                            <RadioButton key={index} label={option} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
         </div>
+
     )
 }
 export default AllRooms
