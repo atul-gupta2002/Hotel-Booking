2fa1f2b8ea5b28691735ff3be2e7e6318e6fa329
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-07-23 00:19:50 +0530

    [Atul Gupta] Updating room details.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import {Route, Routes ,useLocation} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Footer from './components/Footer';
 import AllRooms from "./pages/AllRooms.jsx";
+import RoomDetails from "./pages/RoomDetails.jsx";
 
 const App = () => {
 
@@ -16,6 +17,7 @@ const App = () => {
                 <Routes>
                     <Route path='/' element={<Home/>} />
                     <Route path='/rooms' element={<AllRooms/>} />
+                    <Route path='/rooms/:id' element={<RoomDetails/>} />
                 </Routes>
             </div>
             <Footer />
--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -16,7 +16,7 @@ const CheckBox = ({label, selected = false, onChange= () => { }}) => {
 const RadioButton = ({label, selected = false, onChange= () => { }}) => {
     return (
         <label className='flex gap-3 items-center cursor-pointer mt-2 text-sm'>
-            <input type="radio" name="sortOption" checked={selected} onChange={(e)=>onChange(e.target.checked,label )} />
+            <input type="radio" name="sortOption" checked={selected} onChange={(e)=>onChange(label )} />
             <span className=' font-light select-none'>{label}</span>
         </label>
     )
