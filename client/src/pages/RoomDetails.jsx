f77d2d7ae33a4f9415b1be21d8019a45fb6df7ce
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-07-23 22:32:23 +0530

    More functionality to the hotel rooms are added.

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,28 +1,83 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from "react-router-dom";
-import {roomsDummyData} from "../assets/assets.js";
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+import {assets, facilityIcons, roomsDummyData} from "../assets/assets.js";
+import StarRating from '../components/StarRating.jsx'; // ensure correct path
 
 const RoomDetails = () => {
-    const{id} = useParams()
-    const[rooms, setRooms] = useState(null)
-    const[room, setRoom] = useState(null)
-    const[mainImage, setMainImage] = useState(null)
+    const { id } = useParams();
+    const [room, setRoom] = useState(null);
+    const [mainImage, setMainImage] = useState(null);
 
-    useEffect(()=>{
-        const room=roomsDummyData.find(room => room._id === id)
-        room && setRoom(room)
-        room && setMainImage(room.images[0])
-    },[])
+    useEffect(() => {
+        const selectedRoom = roomsDummyData.find(room => room._id === id);
+        if (selectedRoom) {
+            setRoom(selectedRoom);
+            setMainImage(selectedRoom.images[0]);
+        }
+    }, [id]);
 
     return room && (
-        <div className='py-28 md:py-35 px-4  md:px-16 lg:px-24 xl:px-32 '>
-            {/*Room Details */}
-            <div>
-                <h1>{room.hotel.name} <span>({room.roomType})</span></h1>
-                <p>20% OFF</p>
+        <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+            {/* Room Details */}
+            <div className='flex flex-col md:flex-row items-start md:items-center gap-2'>
+                <h1 className='text-3xl md:text-4xl font-playfair'>
+                    {room.hotel.name} <span className='font-inter text-sm'>({room.roomType})</span>
+                </h1>
+                <p className='text-xs font-inter py-1.5 px-3 text-white bg-orange-500 rounded-full'>20% OFF</p>
             </div>
 
+            {/* Room rating */}
+            <div className='flex items-center gap-1 mt-2'>
+                <StarRating />
+                <p className='ml-2'>200+ reviews</p>
+            </div>
+
+            {/* Room address */}
+            <div className='flex items-center gap-1 text-gray-500 mt-2'>
+                <img src={assets.locationIcon} alt="location-icon" />
+                <span>{room.hotel.address}</span>
+            </div>
+
+            {/*Room images */}
+            <div className='flex flex-col lg:flex-row gap-6 mt-6'>
+                <div className='w-full lg:w-1/2'>
+                    <img src={mainImage} alt="Room image"
+                    className='w-full rounded-xl shadow-lg object-cover' />
+                </div>
+
+                <div className='grid grid-cols-2 gap-4 w-full lg:w-1/2'>
+                    {room?.images.length > 1 && room.images.map((image, index) => (
+                        <img onClick={() => setMainImage(image)}
+                            src={image} alt="Room image" key={index}
+                        className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${mainImage === image && 'outline-3 outline-orange-500'}`} />
+                    ))}
+                </div>
+            </div>
+
+            {/*Room highlights */}
+            <div className='flex flex-col md:flex-row md:justify-between mt-10'>
+                <div>
+                    <h1 className='text-3xl md:text-4xl font-playfair'
+                    >Experience luxury like never before</h1>
+                    <div className='flex flex-wrap items-center gap-4 mt-3 mb-6'>
+                        {room.amenities.map((item,index) => (
+                            <div key={index}>
+                                <img src={facilityIcons[item]} alt={item} className='w-5 h-5' />
+                                <p className='text-xs'>{item}</p>
+
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                {/*Room price */}
+                <p className='text-2xl font-medium'>${room.pricePerNight}/night</p>
+
+            </div>
+
+
         </div>
-    )
+    );
 }
-export default RoomDetails
+
+export default RoomDetails;
