18ca77022767c1749c071b9fbc0f4894d3d6fd6e
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-07-26 13:26:47 +0530

    Compled building the dashboard page and started building the AddRoom page.

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const AddRoom = () => {
+
+    const[images,setImages]=useState({
+        1: null,
+        2: null,
+        3: null,
+        4: null
+
+    })
+
+    const [inputs,setInputs]=useState({
+        roomType: "",
+        pricePerNight: "",
+        amenities: {
+            'Free WiFi': false,
+            'Free parking': false,
+            'Free breakfast': false,
+            'Room service': false,
+            'Mountain view': false,
+            'Pool access': false
+        }
+    })
+
     return (
-        <div>AddRoom</div>
+        <div>
+
+        </div>
     )
 }
 export default AddRoom
