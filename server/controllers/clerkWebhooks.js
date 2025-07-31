c776000e1bbbdecdea7475f2b243e6ca93010665
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-08-01 00:10:03 +0530

    Completed building the schema in User.js file in models.

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,51 @@
+import User from "../models/User.js";
+import {Webhook} from "svix";
+
+const clerkWebhooks = async(req, res)=>{
+    try{
+        const whook  = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        //GETTING HEADERS
+        const headers = {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
+        };
+
+        //Verifying headers
+        await whook.verify(JSON.stringify(req.body),headers)
+
+        //Getting data from request body
+        const{data,type} = req.body
+
+        const userData = {
+            _id: data.id,
+            email: data.email_address[0].email_address,
+            username:data.first_name+" "+data.last_name,
+            image:data.image_url
+
+        }
+
+        //Switch-case for different events
+        switch (type){
+            case "user.created":{
+                await User.create(userData);
+                break;
+            }
+
+
+            default:
+                break;
+        }
+
+
+
+
+
+
+
+    }catch(error){
+
+
+    }
+}
