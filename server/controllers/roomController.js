426ee14bb52adebe95e24c764f3e75eb09cd9f8a
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-08-01 21:45:57 +0530

    Completed building the Room.js file and started building the roomController.js in controllers folder.

---
+++ server/controllers/roomController.js
@@ -0,0 +1,24 @@
+//api to create a new room for a hotel
+
+export const createRoom =async (req,res) => {
+
+}
+
+//api to get ll rooms
+
+export const getRooms =async (req,res) => {
+
+}
+
+//api to get all rooms for a specific hotel
+
+export const getOwnerRooms =async (req,res) => {
+
+}
+
+//api to toggle availability
+
+export const toggleRoomAvailability =async (req,res) => {
+
+}
+
---
+++ server/models/Room.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const roomSchema = mongoose.Schema({
+    roomType: {type: String, required: true},
+    pricePerNight: {type: Number, required: true},
+    amenities: {type: String, required: true},
+    description: {type: Array, required: true},
+    image: {type: String},
+    hotel: {type: String, ref: 'Hotel', required: true},
+
+},{timestamps: true});
+
+const Room = mongoose.model('Room', roomSchema);
+
+export default Room;
