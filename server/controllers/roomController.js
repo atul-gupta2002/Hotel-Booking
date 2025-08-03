71126d30bd6fe65a2ec50b77577fea0c84cbed21
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-08-03 23:46:51 +0530

    roomController.js and Cloudinary.js in configs completed.

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,10 +1,23 @@
+import Hotel from '../models/Hotel.js';
+
 //api to create a new room for a hotel
 
 export const createRoom =async (req,res) => {
+    try{
+        const{roomType , pricePerNight , amenities} = req.body;
+        const hotel = await Hotel.findOne({owner: req.auth.userId});
+
+        if(!hotel){
+            return res.json({success:false,message:"Hotel not found"});
+        }
+    }
+    catch(error){
+        console.log(error.message);
+    }
 
 }
 
-//api to get ll rooms
+//api to get all rooms
 
 export const getRooms =async (req,res) => {
 
