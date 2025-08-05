4d89a41c0eb1dc486e23656b363fa68b554c8b61
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-08-06 00:00:25 +0530

    Completed the Booking.js file in models folder.

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import upload from "../middleware/uploadMIddleware.js";
 import {protect} from "../middleware/authMiddleware.js";
-import {createRoom} from "../controllers/roomController.js";
+import {createRoom, getOwnerRooms, getRooms, toggleRoomAvailability} from
+        "../controllers/roomController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post("/",upload.array("images",4),protect,createRoom);
-
+roomRouter.get("/",getRooms);
+roomRouter.get("/owner",protect,getOwnerRooms);
+roomRouter.post("/toggle-availability",protect,toggleRoomAvailability);
+//we will send the data from the body that's why we are using post
 export default roomRouter;
