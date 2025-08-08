91a1837c1147b5681eda103e34da5600210b22f4
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-08-08 16:03:25 +0530

    Different files changed.

--- client/src/components/HotelReg.jsx
+++ client/src/components/HotelReg.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 import {assets, cities} from "../assets/assets.js";
+import {useAppContext} from "../context/AppContext";
 
 const HotelReg = () => {
+
+     const {setShowHotelReg} = useAppContext();
+
     return (
         <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center
          justify-center bg-black/70'>
@@ -11,7 +15,7 @@ const HotelReg = () => {
 
                 <div className='relative flex flex-col items-center md:w-1/2 p-8 md:p-10'>
                     <img src={assets.closeIcon} alt="close-icon" className='absolute
-                    top-4 right-4 h-4 w-4 cursor-pointer' />
+                    top-4 right-4 h-4 w-4 cursor-pointer' onClick={()=> setShowHotelReg(false)} />
                     <p className='text-2xl font-semibold mt-6'>Register your hotel.</p>
 
                     {/* Hotel name */}
@@ -47,8 +51,8 @@ const HotelReg = () => {
                     {/* Select city drop down */}
                     <div className='w-full mt-4 max-w-60 mr-auto'>
                         <label htmlFor="city" className="font-medium text-gray-500">City</label>
-                        <select id="city" className='className="border border-gray-200 rounded
-                        w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light required "'>
+                        <select id="city" className="border border-gray-200 rounded
+                        w-full px-3 py-2.5 mt-1 outline-indigo-500 font-light required ">
                             <option value="">Select City</option>
                             {cities.map((city) => (
                                 <option key={city} value={city}>{city}</option>
@@ -68,3 +72,4 @@ const HotelReg = () => {
     )
 }
 export default HotelReg
+
